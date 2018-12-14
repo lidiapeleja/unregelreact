e0cbc9188d52af9265c227b3eb2a79aacd97616d
Lídia Pelejà <lidiapeleja@gmail.com>
2018-12-14 12:22:57 +0100

    update some syntax

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -4,8 +4,7 @@ import SignIn from "./SignIn.js";
 import VerbsContainer from "./verbs/VerbsContainer.js";
 import Navbar from "./Navbar.js";
 import VerbsFilling from "./verbs/VerbsFilling.js";
-import Welcome from "./Welcome.js";
-// import Points from "./Points.js";
+import WelcomeContainer from "./WelcomeContainer.js";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 const AppRouter = () => (<Router>
@@ -15,7 +14,7 @@ const AppRouter = () => (<Router>
       <Route path="/" exact="exact" component={VerbsFilling}/>
       <Route path="/sign-in" component={SignIn}/>
       <Route path="/verbs" component={VerbsContainer}/>
-      <Route path="/testing" component={Welcome}/>
+      <Route path="/testing" component={WelcomeContainer}/>
     </div>
   </div>
 </Router>);
