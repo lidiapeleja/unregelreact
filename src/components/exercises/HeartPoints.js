71397f0afda535e9969dbd288f9a8379663bba85
Lidia Peleja <lidiapeleja@gmail.com>
2019-07-28 13:18:13 +0200

    added styling and added flex to hearts and points section

--- src/components/exercises/HeartPoints.js
+++ src/components/exercises/HeartPoints.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Points from "./Points.js";
 import Heart from "./Heart.js";
+import "./HeartPoints.css";
 
 const HeartPoints = props => {
     return (<div>
+      <div className="wrapperheartpoints">
       <Points points={props.points}/>
       <Heart/>
+      </div>
     </div>)
   };
 
