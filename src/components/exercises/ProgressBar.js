f65f25f55205fadf167fbc03d95a78fdd2b67515
Lidia Peleja <lidiapeleja@gmail.com>
2019-08-08 09:01:55 +0200

    added progress bar

---
+++ src/components/exercises/ProgressBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ProgressBar.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+
+const ProgressBar = (props) => {
+
+    return (
+        <div>
+            <div className="progressbar">
+            </div>
+        </div>
+    );
+};
+
+export default ProgressBar;
