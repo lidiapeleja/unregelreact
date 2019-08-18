f00e46c41354854af5922b708d3f7ac5b6c9a5e2
Lidia Peleja <lidiapeleja@gmail.com>
2019-08-18 17:55:07 +0200

    changed in navbar to remove burger

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,32 +6,19 @@ import {faTimes} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faTimes);
 
-const Navbar = (props) => (<nav className="navbar navbar-default">
-  <div className="container-fluid">
-    {/* Brand and toggle get grouped for better mobile display */}
-    <div className="navbar-header">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"/>
-        <span className="icon-bar"/>
-        <span className="icon-bar"/>
-      </button>
+const Navbar = (props) => (<nav className="navbar">
       <a className="navbar-brand" href="http://unregelapp.com/"><h1>UnregelApp</h1><h6>Lern by Playing</h6></a>
-    </div>
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul className="nav navbar-nav">
         <li>
-          <a className="gamelink" href="/exercises/memorize">play!</a>
+          <a className="gamelink" href="/exercises/memorize">New game</a>
         </li>
       </ul>
-      <ul className="nav navbar-nav navbar-right">
+      <ul className="navbar-right">
         <li>
           {/*<FontAwesomeIcon onClick={props.alertExit} className="exitbutton" icon={faTimes}/>*/}
-          <div className="exitbutton" onClick={props.alertExit}>Resume Game</div>
+          <div className="exitbutton" onClick={props.alertExit}>Restart Game</div>
         </li>
       </ul>
-    </div>{/* /.navbar-collapse */}
-  </div>{/* /.container-fluid */}
 </nav>)
 
 
