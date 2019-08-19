facc2f4f57c716d246240fc50e0e15787d26f137
Lidia Peleja <lidiapeleja@gmail.com>
2019-08-19 21:02:45 +0200

    removed cursor pointer from hearts and point

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,7 +8,7 @@ library.add(faTimes);
 
 const Navbar = (props) => (<nav className="navbar">
       <div className="navbar-brand logo" href="http://unregelapp.com/"><a><h1 className="logoh1">UnregelApp</h1><h6 className="logoh6">Lern by Playing</h6></a></div>
-  <div className="navbar-brand newgame"><a href="/exercises/memorize">New game</a></div>
+  <div className="navbar-brand newgame"><a href="/exercises/memorize">New Game</a></div>
           <div className="navbar-brand restart" onClick={props.alertExit}>Restart</div>
 </nav>)
 
