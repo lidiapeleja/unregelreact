e217b57dc1ab16e129bb72288002d7d62e2ea739
Lidia Peleja <lidiapeleja@gmail.com>
2019-08-19 21:16:23 +0200

    links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,13 @@
 import "./Navbar.css";
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {faTimes} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faTimes);
 
 const Navbar = (props) => (<nav className="navbar">
-      <div className="navbar-brand logo" href="http://unregelapp.com/"><a><h1 className="logoh1">UnregelApp</h1><h6 className="logoh6">Lern by Playing</h6></a></div>
-  <div className="navbar-brand newgame"><a href="/exercises/memorize">New Game</a></div>
+      <div className="navbar-brand logo"><a href="http://unregelapp.com/"><h1 className="logoh1">UnregelApp</h1><h6 className="logoh6">Lern by Playing</h6></a></div>
+  <div className="navbar-brand newgame" onClick={props.alertExit}>New Game</div>
           <div className="navbar-brand restart" onClick={props.alertExit}>Restart</div>
 </nav>)
 
