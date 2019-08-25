d14c58ec5516816bc9e6ac01ad4b411aa0b5f681
Lidia Peleja <lidiapeleja@gmail.com>
2019-08-25 20:04:28 +0200

    added points word to points

--- src/components/exercises/Points.js
+++ src/components/exercises/Points.js
@@ -4,7 +4,7 @@ import "./Points.css";
 
 const Points = props => {
   return <div className="container-points">
-    <div className="points animated bounceIn delay-1s">
+    <div className="points animated bounceIn delay-1s"><span className="spanpoints">Points: </span>
       {props.points}
     </div>
   </div>
