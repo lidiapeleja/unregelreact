973422e94d007d633ad24055dba5186bd465331f
Lidia Peleja <lidiapeleja@gmail.com>
2019-08-31 11:36:44 +0200

    added htpps to a logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,7 +6,7 @@ import {faTimes} from "@fortawesome/free-solid-svg-icons";
 library.add(faTimes);
 
 const Navbar = (props) => (<nav className="navbar">
-      <div className="navbar-brand logo"><a href="http://unregelapp.com/"><h1 className="logoh1">UnregelApp</h1><h6 className="logoh6">Lern by Playing</h6></a></div>
+      <div className="navbar-brand logo"><a href="https://unregelapp.com/"><h1 className="logoh1">UnregelApp</h1><h6 className="logoh6">Lern by Playing</h6></a></div>
   <div className="navbar-brand newgame" onClick={props.alertExit}>New Game</div>
           <div className="navbar-brand restart" onClick={props.alertExit}>Restart</div>
 </nav>)
