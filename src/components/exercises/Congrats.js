ba5ebd15ca70ccf2381f1cd36d4a7779ed658a9d
Lidia Peleja <lidiapeleja@gmail.com>
2019-09-01 23:36:25 +0200

    fixed styling

--- src/components/exercises/Congrats.js
+++ src/components/exercises/Congrats.js
@@ -8,9 +8,9 @@ const Congrats = (props) => (<nav className="container">
     <div className="congrats">
         <div className="animation animated pulse infinite"><h2>Congratulations!</h2>
         <h4 className="displaypoints">Final Points: {props.points}</h4>
-            <a className="whatsappshare" href="whatsapp://send?text=Hi! I just learned the 25 most important german irreglua UnregelApp helps everyone to learn the German irregular verbs - try it https://unregelapp.com " data-action="share/whatsapp/share">Share it<img className="whatsapp" src={whatsapp} alt="whatsapp" />
-            </a>
         </div>
+        <a className="whatsappshare" href="whatsapp://send?text=I just learned the 25 most important german irregular verbs with UnregelApp - try it https://unregelapp.com " data-action="share/whatsapp/share">Share it<img className="whatsapp" src={whatsapp} alt="whatsapp" />
+        </a>
     </div>
 </nav>)
 
