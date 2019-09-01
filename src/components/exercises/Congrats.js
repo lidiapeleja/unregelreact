b1abd00bdb4656576a4fe75e31cc4b79cf484134
Lidia Peleja <lidiapeleja@gmail.com>
2019-09-01 20:21:59 +0200

    added whatsapp share

--- src/components/exercises/Congrats.js
+++ src/components/exercises/Congrats.js
@@ -5,7 +5,8 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 const Congrats = (props) => (<nav className="container">
     <div className="congrats">
         <div className="animation animated pulse infinite"><h2>Congratulations!  😎</h2>
-        <h4 className="displaypoints">Total of Points: {props.points}</h4>
+        <h4 className="displaypoints">Total of Points: {props.points}        </h4>
+            <a className="whatsappshare" href="whatsapp://send?text=Hi! I made it! UnregelApp helps everyone to learn the German irregular verbs - try it https://unregelapp.com " data-action="share/whatsapp/share">Share it via Whatsapp</a>
         </div>
     </div>
 
