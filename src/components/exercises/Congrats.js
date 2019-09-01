6a5ac14e32ee42f8cd62ec7e9bdfa90841da4a8a
Lidia Peleja <lidiapeleja@gmail.com>
2019-09-01 23:56:07 +0200

    fixed more styling

--- src/components/exercises/Congrats.js
+++ src/components/exercises/Congrats.js
@@ -6,10 +6,10 @@ import whatsapp from './whatsapp.svg';
 
 const Congrats = (props) => (<nav className="container">
     <div className="congrats">
-        <div className="animation animated pulse infinite"><h2>Congratulations!</h2>
-        <h4 className="displaypoints">Final Points: {props.points}</h4>
+        <div className="animation animated pulse infinite"><h2> 🎈💥 Congratulations! 💥🎈</h2>
+        <h4 className="displaypoints">Final Points: {props.points} </h4>
         </div>
-        <a className="whatsappshare" href="whatsapp://send?text=I just learned the 25 most important german irregular verbs with UnregelApp - try it https://unregelapp.com " data-action="share/whatsapp/share">Share it<img className="whatsapp" src={whatsapp} alt="whatsapp" />
+        <a className="whatsappshare" href="whatsapp://send?text= Hey! 🏆 I just learnt the 25 most important german irregular verbs with UnregelApp! Try it ➡️ https://unregelapp.com " data-action="share/whatsapp/share">Share it<img className="whatsapp" src={whatsapp} alt="whatsapp" />
         </a>
     </div>
 </nav>)
