6e187081e21139e32a9801c1c3a74d252d717c51
Lidia Peleja <lidiapeleja@gmail.com>
2019-09-01 20:15:39 +0200

    added animated to congrats

--- src/components/exercises/Congrats.js
+++ src/components/exercises/Congrats.js
@@ -4,7 +4,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Congrats = (props) => (<nav className="container">
     <div className="congrats">
-        <div className="animation animated bounceIn delay-1s "><h2>Congratulations!  😎</h2>
+        <div className="animation animated pulse infinite"><h2>Congratulations!  😎</h2>
         <h4 className="displaypoints">Total of Points: {props.points}</h4>
         </div>
     </div>
