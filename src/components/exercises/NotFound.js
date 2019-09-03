922c02ffce6e5c274bbe6a24c02abd04809c774f
Lidia Peleja <lidiapeleja@gmail.com>
2019-09-03 22:31:15 +0200

    404 error styling

--- src/components/exercises/NotFound.js
+++ src/components/exercises/NotFound.js
@@ -3,8 +3,8 @@ import "./NotFound.css";
 
 const NotFound = props => {
     return (
-        <div className="container notfound">
-            <p>Ups, something with your URL seems wrong. Go back to Home</p>
+        <div className="container notfound animated rubberBand">
+            <p><span>Ups,</span> there is not so much going on in here. Go back to Home.</p>
         </div>
     );
 };
