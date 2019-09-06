537877ad2b048bab7e070ee617a1663432e4ba37
Lidia Peleja <lidiapeleja@gmail.com>
2019-09-07 00:01:14 +0200

    final style in congrats

--- src/components/exercises/Congrats.js
+++ src/components/exercises/Congrats.js
@@ -3,12 +3,11 @@ import React from "react";
 
 const Congrats = (props) => (<nav className="container">
     <div className="congrats">
-        <div><h2 className="animation animated pulse"> 🎈 Congratulations! 🎈</h2>
+        <h2 className="animation animated pulse"> 🎈 Congratulations! 🎈</h2>
             <h4 className="displaypoints">Final Points: {props.points} </h4>
-            <a type="submit" className="btn whatsappshare textbutton nonactiv animated bounceIn delay-2s" href="whatsapp://send?text= Hey! 🏆 I just learnt the 25 most important german irregular verbs with UnregelApp! Try it ➡️ https://unregelapp.com " data-action="share/whatsapp/share">Share it on Whatsapp</a>
-            <a type="submit" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Funregelapp.com" target="_blank" rel="noopener" className="btn facebookshare textbutton nonactiv animated bounceIn delay-2s">Share it on Facebook</a>
-            <a type="submit" className="btn twittershare textbutton nonactiv animated bounceIn delay-2s" href="https://twitter.com/share?url=https%3A%2F%2Funregelapp.com%2F&text=I just learnt the 25 most important german irregular verbs with UnregelApp ! Try it ➡ ️">Share it on Twitter</a>
-        </div>
+            <a type="submit" className="btn whatsappshare textbutton nonactiv animated fadeIn delay-2s" href="whatsapp://send?text= Hey! 🏆 I just learnt the 25 most important german irregular verbs with UnregelApp! Try it ➡️ https://unregelapp.com " data-action="share/whatsapp/share">Share it on Whatsapp</a>
+            <a type="submit" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Funregelapp.com" target="_blank" rel="noopener" className="btn facebookshare textbutton nonactiv animated fadeIn delay-2s">Share it on Facebook</a>
+            <a type="submit" className="btn twittershare textbutton nonactiv animated fadeIn delay-2s" href="https://twitter.com/share?url=https%3A%2F%2Funregelapp.com%2F&text=I just learnt the 25 most important german irregular verbs with UnregelApp ! Try it ➡ ️">Share it on Twitter</a>
     </div>
 </nav>);
 
