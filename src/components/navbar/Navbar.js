54ae8d90c9e5c6e19dfc2c1074e488d8a4cafe4c
Lidia Peleja <lidiapeleja@gmail.com>
2019-09-22 20:33:10 +0200

    added Quit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -14,22 +14,22 @@ const Navbar = withRouter((props) => (<nav className="navbar">
     <div className="navbar-brand logo"><a href="https://unregelapp.com/"><h1 className="logoh1">UnregelApp</h1><h6
         className="logoh6">Lern by Playing</h6></a></div>
     <div className="navbar-brand newgame" onClick={() => alertNewGame(props.resetState, props.history)}>New Game</div>
-    <div className="navbar-brand restart" onClick={() => alertReset(props.resetState, props.history)}>Restart</div>
+    <div className="navbar-brand restart" onClick={() => alertQuit(props.resetState, props.history)}>Quit</div>
 </nav>));
 
 
-function alertReset(resetState, routerHistory) {
+function alertQuit(resetState, routerHistory) {
     MySwal.fire({
-        title: "Are you sure <br> you want to Restart?",
+        title: "Are you sure <br> you want to Quit?",
         text: "All current progress will be lost",
-        confirmButtonText: 'Restart',
+        confirmButtonText: 'Quit',
         showCancelButton: true,
         background: '#ffde03',
         confirmButtonColor: '#ff0266'
     }).then((result) => {
             if (result.value) {
                 resetState();
-                routerHistory.push('/loading');
+                window.location.href = 'https://unregelapp.com' ;
             }
         }
     );
